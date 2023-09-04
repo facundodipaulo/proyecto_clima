afe6701f9cfc6396a0a46cf11247a991d4ce3cb1
facundodipaulo <141045729+facundodipaulo@users.noreply.github.com>
2023-09-04 14:34:46 -0300

    Add files via upload

---
+++ index_script.js
@@ -0,0 +1,44 @@
+let longitud;
+let latitud;
+let temperatura = document.querySelector(".temperatura");
+let descripcion = document.querySelector(".descripcion");
+let hubicacion = document.querySelector(".hubi");
+let imagen = document.querySelector(".imagen");
+const kelvin = 273.15;
+
+window.addEventListener("load", function () {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+            console.log(position);
+            longitud = position.coords.longitude;
+            latitud = position.coords.latitude;
+            const api_key = "da37d295703cc254fdec5629780887dc";
+            const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitud}&lon=${longitud}&appid=${api_key}`;
+            fetch(url)
+                .then((response) => {
+                    return response.json();
+                })
+                .then((data) => { 
+                    
+                    console.log(data);
+                    temperatura.textContent = Math.floor(data.main.temp - kelvin) + "°C";
+                    descripcion.textContent = data.weather[0].description; 
+                    hubicacion.textContent = data.name + "," + data.sys.country;
+
+
+                    const iconCode = data.weather[0].icon;
+                    iconUrl = `https://openweathermap.org/img/wn/${iconCode}.png`;
+                    imagen.src = iconUrl;
+                    console.log(iconUrl)
+                    console.log(iconCode)
+
+
+                })
+                .catch((error) => {
+                    console.error("Error al obtener los datos del clima");
+                });
+        });
+    } else {
+        console.error("Geolocalización no es compatible en este navegador.");
+    }
+});
